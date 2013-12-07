452938d5355dbf76cee96ca7f97b0f91a20111e6
Sheld <sheld@debian.local>
2013-12-06 19:20:08 -0800

    Restructure - less creation/destruction and more portability

--- bin/dbRead.py
+++ bin/dbRead.py
@@ -6,13 +6,12 @@ import sys
 import networkx as nx
 
 
-con = mdb.connect('localhost', 'databaseHandler', 'test123', 'evedb');
-map = nx.Graph()
+
 
 def getRows (cur, location):
-	sql = "select mapSolarSystemJumps.toSolarSystemID from mapSolarSystemJumps inner join " + \
-	"mapSolarSystems on mapSolarSystemJumps.fromSolarSystemID=mapSolarSystems.solarSystemID " + \
-	"where mapSolarSystems.solarSystemName ='" + location + "'"
+	sql = "SELECT mapSolarSystemJumps.toSolarSystemID FROM mapSolarSystemJumps INNER JOIN " + \
+	"mapSolarSystems ON mapSolarSystemJumps.fromSolarSystemID=mapSolarSystems.solarSystemID " + \
+	"WHERE mapSolarSystems.solarSystemName ='" + location + "'"
 	cur.execute(sql)
 	rows = cur.fetchall()
 	return rows	
@@ -26,6 +25,7 @@ def nameToID (cur, name):
 def buildMap (cur):
 #Builds a networkx graph represting a map we will use for our calculations
 	cur.execute("SELECT solarSystemID FROM mapSolarSystems")
+	map = nx.Graph()
 	solarSystems = cur.fetchall()
 	for system in solarSystems:
 		map.add_node(system[0])
@@ -33,35 +33,31 @@ def buildMap (cur):
 	jumps = cur.fetchall()
 	for i in jumps:
 		map.add_edge(i[0], i[1])
-	
-with con:
-	
-	start = raw_input("Where are you? > ")
-	end = raw_input("where are you going > ")
-	
-	cur = con.cursor()
-	location = nameToID (cur, start)
-	dest = nameToID (cur, end)
-	sql = "SELECT solarSystemID from mapSolarSystems where solarSystemName = '" + start + "'"
-	cur.execute(sql)
-	sID = cur.fetchone()
-	print "System ID : %s " % sID
-	
-	#This will get all adjacent solar system IDs from the location
-#	rows = getRows(cur, start)
-#	for i in rows:
-#		print i[0]
-	
-	buildMap(cur)
-	
-	path = nx.shortest_path(map, location[0], dest[0])
-	distance = len(path)-1
+	return map
 
-
-   
-	
-	
-
-	
+def shortestPath (con, map, start, end):
+		
+	with con:
+		cur = con.cursor()
+		beg = nameToID (cur, start)
+		dest = nameToID (cur, end)
+		sql = "SELECT solarSystemID from mapSolarSystems where solarSystemName = '" + start + "'"
+		cur.execute(sql)
+		sID = cur.fetchone()
+		path = nx.shortest_path(map, beg[0], dest[0])
+		distance = len(path)-1
+		return distance
+		
 def checkMed (cur, station): #Returns true if a given station has cloning services returning true all the time right now
-	return TRUE
+	return TRUE	
+	
+con = mdb.connect('localhost', 'databaseHandler', 'test123', 'evedb');
+with con:
+	c = con.cursor()
+	map = buildMap(c)
+
+start = raw_input("Where are you? > ")
+end = raw_input("where are you going > ")
+
+print shortestPath(con, map, start, end)
+print "end"
