4d083a7ac508326601d13c1221376cf540d1bffb
sheld <sheld@debian.local>
2013-12-06 16:08:49 -0800

    Put in a location, get the systems around it

--- bin/dbRead.py
+++ bin/dbRead.py
@@ -5,6 +5,7 @@ import MySQLdb as mdb
 import sys
 
 location = raw_input("Where are you? > ")
+#dest = raw_input("where are you going >")
 
 con = mdb.connect('localhost', 'databaseHandler', 'test123', 'evedb');
 	
@@ -13,5 +14,14 @@ with con:
 	sql = "SELECT solarSystemID from mapSolarSystems where solarSystemName = '" + location + "'"
 	cur.execute(sql)
 	sID = cur.fetchone()
+	print "System ID : %s " % sID
+	
+	sql = "select mapSolarSystemJumps.toSolarSystemID from mapSolarSystemJumps inner join mapSolarSystems on mapSolarSystemJumps.fromSolarSystemID=mapSolarSystems.solarSystemID where mapSolarSystems.solarSystemName ='" + location + "'"
+	cur.execute(sql)
+	rows = cur.fetchall()
+	for i in rows:
+		print i[0]
    
-	print "System ID : %s " % sID    
+	
+	
+# what I want to do is to select the name of all gates where the from is my location
