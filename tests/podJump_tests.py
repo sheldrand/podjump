cf8161f9b239442f679ae9899abcaf96bfe39840
sheldrand <joyfultravler@gmail.com>
2013-12-11 14:10:03 -0800

    starting the tests

--- tests/podJump_tests.py
+++ tests/podJump_tests.py
@@ -3,7 +3,10 @@ import podJump.py
 
 def test_router():
 	router = pathFinder()
-	assert_equal(router.con
+	assertIsNotNone(router.con)
+	assertIsNotNone(router.map)
+	assertEqual(router.map.number_of_nodes(), 5431)
+	assertEqual(router.map.number_of_edges(), 5) #will currently fail, need to find the actual number of possible jumps
 
 def setup():
     print "SETUP!"
