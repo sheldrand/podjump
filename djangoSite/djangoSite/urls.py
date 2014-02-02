b8b756bf17bc11ec0a566e356917005044322e2c
Sheld <joyfultravler@gmail.com>
2014-02-02 10:23:48 -0800

    hour ahead

--- djangoSite/djangoSite/urls.py
+++ djangoSite/djangoSite/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from djangoSite.views import hello, current_time
+from djangoSite.views import hello, current_time, hour_ahead
 
 from django.contrib import admin
 admin.autodiscover()
@@ -12,4 +12,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 	url(r'^hello/$', hello),
 	url(r'^t/$', current_time),
+	url(r'^time/plus/(\d{1,2})+/$', hour_ahead),
 )
--- djangoSite/djangoSite/views.py
+++ djangoSite/djangoSite/views.py
@@ -7,4 +7,13 @@ def hello(request):
 def current_time(request):
 	now = datetime.datetime.now()
 	html="<html><body>It is now %s.</body></html>" % now
-	return HttpResponse(html)	
+	return HttpResponse(html)
+
+def hour_ahead(request, offset):
+	try:
+		offset = int(offset)
+	except ValueError:
+		raise Http404()
+	dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+	html="<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+	return HttpResponse (html)
