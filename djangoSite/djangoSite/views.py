ff56764862920a50862994fcd9a2e9edb3562531
Sheld <joyfultravler@gmail.com>
2014-02-02 11:06:02 -0800

    render

--- djangoSite/djangoSite/views.py
+++ djangoSite/djangoSite/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template.loader import get_template
 from django.template import Context
+from django.shortcuts import render
 import datetime
 
 def hello(request):
@@ -22,6 +23,7 @@ def hour_ahead(request, offset):
 
 def current_dt(request):
 	now = datetime.datetime.now()
-	t = get_template('current_datetime.html')
-	html = t.render(Context({'current_date':now}))
-	return HttpResponse(html)
+	return render(request, 'current_datetime.html', {'current_date':now})
+#	t = get_template('current_datetime.html')
+#	html = t.render(Context({'current_date':now}))
+#	return HttpResponse(html)
