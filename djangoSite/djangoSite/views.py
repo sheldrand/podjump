81d18a1ecd51d020564816eca3e17e3e09afe59f
Sheld <joyfultravler@gmail.com>
2014-02-02 11:26:35 -0800

    new templates

--- djangoSite/djangoSite/views.py
+++ djangoSite/djangoSite/views.py
@@ -18,8 +18,9 @@ def hour_ahead(request, offset):
 	except ValueError:
 		raise Http404()
 	dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-	html="<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
-	return HttpResponse (html)
+	return render(request, 'hour_ahead.html', {'offset_time':dt, 'hours':offset})
+#	html="<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+#	return HttpResponse (html)
 
 def current_dt(request):
 	now = datetime.datetime.now()
