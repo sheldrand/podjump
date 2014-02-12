e3312cc3b1c1b2b2126898d56da377f2156e5f52
Ben McKinley <bmckinley@groupon.com>
2014-02-12 13:49:53 -0800

    setting up the forms

--- djangoSite/podJump/views.py
+++ djangoSite/podJump/views.py
@@ -14,8 +14,20 @@ def search_form (request):
         form = SearchForm()
     return render(request, 'search_form.html',{'form':form})
 
-def show_connections (request, places):
-    stations = parceplaces(places)
+def show_connections (request):
+	#Form passes directly here so we need to check if the data is valid.
+	#If not, we send them back to the form page (is this the right way to do it?)
+	#places not used in this implementation
+	if (request.method == 'POST'):
+		form = SearchForm(request.POST)
+		if form.is_valid():
+			assert False
+			data = form.cleaned_data
+		else:
+			return HttpResponseRedirect('/') #should send home
+#	stations = parceplaces(data)
+	return render(request, 'show.html')
+	
 
 
 def parceplaces (places):
