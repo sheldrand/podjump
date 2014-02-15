bfa0d9b9c67b082255e4651d81d550b3f3d8b595
Sheld <joyfultravler@gmail.com>
2014-02-14 16:23:23 -0800

    Using for loops to keep memory usage lower time to run

--- mapconvert/DBconvert/converter.py
+++ mapconvert/DBconvert/converter.py
@@ -36,40 +36,15 @@ with con:
                 if nx.has_path(map, source[0], dest[0]):
                     jumps = len(nx.shortest_path(map, source[0], dest[0])) - 1
                     if jumps > 0:
-                        id+=int(1)
+                        id+=1
 #                    print "INSERT INTO routing_info VALUES ({!s},{!s},{!s},{!s})".format(id, source, destination, jumps)
-                        build.execute("INSERT INTO routing_info VALUES (?,?,?,?)", (id, source, destination, jumps))
+                        build.execute("INSERT INTO routing_info VALUES (?,?,?,?)", (id, source[1], dest[1], jumps))
+
 
 
 #                    print "Going from %s to %s and is %s jumps" % (source[1], dest[1], jumps)
 
-        print "getting paths"
-        data = nx.all_pairs_shortest_path(map)
-        print "paths aquired"
-        id = int(0)
-        build.execute("DROP TABLE IF EXISTS routing_info")
-        build.execute("CREATE TABLE routing_info(Id INT, from_system TEXT, to_system TEXT, num_jumps INT)")
-        for i in data:
-    #        print i[1]
-            routes = data.get(i)
-            source = map.node[i].get('name')
-            print map.node[i].get('name')
-            for route in routes:
-                destination = map.node[route].get('name')
-                jumps = len(routes[route])-1
-                if jumps > 0:
-                    id+=int(1)
-#                    print "INSERT INTO routing_info VALUES ({!s},{!s},{!s},{!s})".format(id, source, destination, jumps)
-                    build.execute("INSERT INTO routing_info VALUES (?,?,?,?)", (id, source, destination, jumps))
-#                    new.commit()
 
-#        build.execute("SELECT * FROM routing_info")
-#        rows = build.fetchall()
-#        for row in rows:
-#            print row
-
-
-#    print data
 
 print "if all else fails"
 # new database layout: id, from system, to system, # jumps
